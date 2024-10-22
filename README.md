4a5713e6e99a7ce27059147058c3e346adbe5fff
Alan <quanhwalker1999@gmail.com>
2024-10-22 11:22:30 +0900

    Added mode switching, updated README file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-SpyCAM
+![Python](https://img.shields.io/badge/python-3.8.8-3670A0?style=for-the-badge&logo=python&logoColor=white)
+![PyTorch](https://img.shields.io/badge/PyTorch-1.7.1-%23EE4C2C.svg?style=for-the-badge&logo=PyTorch&logoColor=white)
+![scikit-learn](https://img.shields.io/badge/scikit--learn-0.24.1-%23F7931E.svg?style=for-the-badge&logo=scikit-learn&logoColor=white)
+[[Project]](https://github.com/zachnguyen03/opencv_spycam)
+
+# OpenCV Spycam - Official Implementation
+This is the official code of our **"SCMIT: Style Consistent Multi-Domain Image-to-Image Translation"**, TBD, 2024
+
+## **Requirements**
+-opencv-python>=4.9.0.80
+
+## **Initialzing spycam**
+1. Webcam mode
+`python main.py --mode webcam`
+2. Video mode
+`python main.py --mode video`
+
+## **Parameters**
+1. **IP address**: used to specify the destination for cv2.VideoCapture
+- Set to 0: OpenCV capture your computer's webcam
+- Set to a RTSP address: OpenCV captures the camera (e.g CCTV) from the specified RTSP IP address
+2. **Codec**: specify the codec which cv2.VideoWriter uses to make the output video
+- Available codecs: 'MJPG' (recommended for 'avi'), 'DVIX', 'MP4V' 
+3. **FPS**: specify the FPS of the output video (default: 10.0)
+4. **Mode**: specify the starting mode of the VideoCapture 
+- This mode can be changed during the capture by pressing SPACE key
+
+## **Features**
+1. Applying filter to VideoCapture - In progress
+
+
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2
+import argparse
 
 class Webcam:
-    def __init__(self, ip):
+    def __init__(self, ip, codec):
         self.ip = ip
         self.cam = cv2.VideoCapture(ip)
         if (self.cam.isOpened() == False):
             print("Error reading video file")
         self.mode = 'webcam'
-        self.fourcc = int(cv2.VideoWriter_fourcc(*'MJPG'))
+        self.fourcc = int(cv2.VideoWriter_fourcc(*codec))
         self.cam_width = self.cam.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.cam_height = self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
         self.fps = self.cam.get(cv2.CAP_PROP_FPS)
@@ -16,7 +17,7 @@ class Webcam:
 
     def switch_mode(self):
         if self.mode == 'webcam':
-            self.mode = 'video'
+            self.mode = 'record'
         else:
             self.mode = 'webcam'
 
@@ -25,21 +26,31 @@ class Webcam:
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ip', type=str, default='rtsp://210.99.70.120:1935/live/cctv001.stream', help='specify IP or RTSP address for VideoCapture')
+    parser.add_argument('--fps', type=float, default=20.0, help='specify FPS of output video')
+    parser.add_argument('--save_path', type=str, default='out.avi', help='specify destination to save video')
+    parser.add_argument('--mode', type=str, default='webcam', help='specify starting mode of SpyCAM')
+    parser.add_argument('--fourcc', type=str, default='MJPG', help='specify codec for output video')
+    
+    args = parser.parse_args()
+    
     # Load camera Cheonan traffic
-    ip = 'rtsp://210.99.70.120:1935/live/cctv001.stream'
-    webcam = Webcam(ip)
-    vid = cv2.VideoWriter('traffic.avi', webcam.fourcc, 10.0, (int(webcam.cam_width), int(webcam.cam_height)), isColor=True)
+    webcam = Webcam(args.ip, args.fourcc)
+    vid = cv2.VideoWriter(args.save_path, webcam.fourcc, args.fps, (int(webcam.cam_width), int(webcam.cam_height)), isColor=True)
 
 
     while webcam.cam.isOpened():
         _, frame = webcam.cam.read()
-        frame = cv2.resize(frame, (640, 480))
+        # frame = cv2.resize(frame, (640, 480))
         cv2.imshow("Webcam", frame)
-        vid.write(frame)
-        if cv2.waitKey(1) & 0xFF == 27: # press ESC
+        if webcam.mode == 'record':
+            vid.write(frame)
+        if cv2.waitKey(1) & 0xFF == 32:
+            webcam.switch_mode()
+            print(f"Switched to mode: {webcam.mode}")
+        elif cv2.waitKey(1) & 0xFF == 27: # press ESC
             break
-        elif cv2.waitKey(1) & 0xFF == 32: # press Space
-            pass
 
     webcam.cam.release()
     vid.release()
